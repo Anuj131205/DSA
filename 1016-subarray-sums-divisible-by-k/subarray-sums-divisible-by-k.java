819b32769fbdf6e800d2221e4db00102b4ad897f
ANUJ KUMAR <kumarsinghanuj42@gmail.com>
2026-09-07 23:39:47 +0530

    Time: 23 ms (81.76%) | Memory: 55.1 MB (6.95%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
@@ -1,4 +1,36 @@
+import java.util.HashMap;
+
 class Solution {
+    public int subarraysDivByK(int[] a, int k) {
+        int sum = 0;
+        int res = 0;
+        HashMap<Integer, Integer> freq = new HashMap<>();
+
+        // remainder 0 initially ek baar
+        freq.put(0, 1);
+
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+            int rem = sum % k;
+
+            // Java mein negative remainder aa sakta hai
+            if (rem < 0) {
+                rem += k;
+            }
+
+            // Same remainder pehle kitni baar aaya?
+            res += freq.getOrDefault(rem, 0);
+
+            // Current remainder ki frequency badhao
+            freq.put(rem, freq.getOrDefault(rem, 0) + 1);
+        }
+
+        return res;
+    }
+}
+
+
+/* class Solution {
     public int subarraysDivByK(int[] a, int k) {
 
         int sum = 0;
@@ -25,3 +57,4 @@ class Solution {
         return res;
     }
 }
+*/
